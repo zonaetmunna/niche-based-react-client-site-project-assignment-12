55435d4645e84ba1620e198ea64a754e73dd403b
zonaetmunna <zonaetmonna@gmail.com>
2021-11-12 19:34:56 +0600

    add

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -9,23 +9,3 @@ const firebaseConfig = {
 
 export default firebaseConfig;
 
-/* const firebaseConfig = {
-     apiKey: "AIzaSyBwIiLOKaVKJqLIiimRCgVO07vl6jpVs-I",
-     authDomain: "the-hands-of-time.firebaseapp.com",
-     projectId: "the-hands-of-time",
-     storageBucket: "the-hands-of-time.appspot.com",
-     messagingSenderId: "202285767464",
-     appId: "1:202285767464:web:d277b82edcfcf42a9a8e1d"
-};
-export default firebaseConfig; */
-
-/* const firebaseConfig = {
-     apiKey: "AIzaSyCZD0IzVdZQEdqvb84YqdB4LFtBreJBQ3k",
-     authDomain: "react-firebase-private-router.firebaseapp.com",
-     projectId: "react-firebase-private-router",
-     storageBucket: "react-firebase-private-router.appspot.com",
-     messagingSenderId: "521087828215",
-     appId: "1:521087828215:web:2ec03e4bfea265a3aa53f2"
-};
-
-export default firebaseConfig; */
